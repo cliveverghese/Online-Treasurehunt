b35f2b60667fba59c879fd8aeaa07bbb9d704b81
Clive Verghese <cliveverghese@gmail.com>
2011-08-20 00:09:10 +0530

    modified check.php

--- check.php
+++ check.php
@@ -1,20 +1,6 @@
 <?php
-	require_once('database.php');
-			$to = 'cliveverghese@gmail.com';
-			$subject = 'Registered users';
-		$result = mysql_query("SELECT * FROM logs");
-		$content = "\n";
-while($array = mysql_fetch_array($result)) {
-	$content = $content .  '\n' . $array['user'] . ' ' . $array['val'] ;
-}
-$list[0] = "cliveverghese@gmail.com";
-	$message = $content;
-	$content = "message sent";
-	foreach($list as $to)
-	{
-		$headers = "From: admin@tresor.conjura.in" . "\r\n" . "Reply-To: cliveverghese@gmail.com";
-		mail($to, $subject, $message, $headers);
-		$content = $content . $to . "<br>";
-	}
+/*this file was used for testing purpose. 
+and to retrieve some of the logs from the server 
+*/
 ?>
 
